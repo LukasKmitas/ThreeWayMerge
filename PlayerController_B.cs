64bceb8bca43568e2a63bcbae0e94a705062e4b8
Lukas Kmitas <lukaskmitas@hotmail.co.uk>
2023-10-02 10:26:32 +0100

    branch_b

---
+++ PlayerController_B.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed = 5f;
+
+	void Update()
+	{
+    	float moveHorizontal = Input.GetAxis("Horizontal");
+    	float moveVertical = Input.GetAxis("Vertical");
+
+    	Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0.0f); // changed movement vector
+    	transform.Translate(movement * speed * Time.deltaTime);
+	}
+}
